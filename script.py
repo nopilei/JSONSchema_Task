02d74c805450a395e1d1569d19410c1bf0425f59
rtconverter <rtconverter@gmail.com>
2020-11-24 21:13:20 +0300

    fixed output

--- script.py
+++ script.py
@@ -33,32 +33,34 @@ def main():
     logging.config.dictConfig(dict_log_config)
     logger = logging.getLogger("App")
 
+    #  Load events from event dir
     events: Dict[str, dict] = {}  # Dict for mapping event filenames to event instances
     for filename in os.listdir('event'):
         with open(os.path.join('event', filename)) as file:
             events[filename] = json.load(file)
 
+    #  Load schemas from schema dir
     schemes: Dict[str, dict] = {}  # Dict for mapping schema filenames (without .schema) to schema instances
     for filename in os.listdir('schema'):
         with open(os.path.join('schema', filename)) as file:
             schemes[filename[:filename.find('.')]] = json.load(file)
 
-    for event_filename, event in events.items():
+    for ix, (event_filename, event) in enumerate(events.items(), start=1):
         if event is None:
-            logger.info(f"'{event_filename} has not corresponding json schema!")
+            logger.info(f"{ix}. '{event_filename}' has not corresponding json schema!")
         else:
             event_name = event.get('event', None)
         if event_name is None:
-            logger.info(f"Field 'event' is absent in '{event_filename}'!")
+            logger.info(f"{ix}. Field 'event' is absent in '{event_filename}'!")
         elif event_name not in schemes:
-            logger.info(f"{event_filename} has not corresponding json schema!"
+            logger.info(f"{ix}. {event_filename} has not corresponding json schema!"
                         f" Check the 'event' field in {event_filename}")
         else:
             schema = schemes[event_name]
             try:
                 jsonschema.validate(event, schema)
             except jsonschema.exceptions.ValidationError as err:
-                logger.info(f"Validation error when tried to apply"
+                logger.info(f"{ix}. Validation error when tried to apply"
                             f" '{event_name}.schema' schema on '{event_filename}'!"
                             f" {err.message}")
 
